f84bc6a5243f167a6fbcaebd0935133bfc5e9019
edisonw98 <73946718+edisonw98@users.noreply.github.com>
2021-11-04 16:10:34 +0100

    Create User.py

---
+++ User.py
@@ -0,0 +1,31 @@
+class User:
+
+    userid;
+    generate;
+    blackout;
+    turbbine;
+    usage;
+
+    #creates new customer should contain stuff
+    def __init__(self):
+        pass
+
+    #sell electricity only if you have a turbine
+    def sell (turbine):
+        pass
+    
+    #Takes in wind from system to generate fixed amount
+    def generate():
+        pass
+
+    #buy electricity
+    def buy(blackout):
+        pass
+
+    # if a user builds a turbine after creation
+    def buildturbine(turbine):
+        self.turbine = True
+
+
+
+
