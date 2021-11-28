eca7d1a73b755a8c31d9309529c0f3b1a1899c86
edisonw98 <73946718+edisonw98@users.noreply.github.com>
2021-11-28 18:16:26 +0100

    Registration from webpage to personal dev server works
    fixed for my personal mysql server will fix for shared one asap

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,46 +1,56 @@
-import { useState, useEffect} from 'react';
+
+import { useState} from 'react';
 import './App.css';
 import Axios from 'axios';
 
 
 function App() {
 
-  const[name, setName] = useState('')
-  const[adress, setAdress] = useState('')
-  const[zip, setZip] = useState('')
-  const[password, setPassword] = useState('')
-  const[email, setEmail] = useState('')
+  const[name, setName] = useState("");
+  const[adress, setAdress] = useState("");
+  const[zip, setZip] = useState(0);
+  const[password, setPassword] = useState("");
+  const[email, setEmail] = useState("");
 
   const submitRegistration = () => {
-    Axios.post('http://localhost:3008/')
-  }
+    Axios.post("http://localhost:3001/register", {
+      name: name,
+      adress: adress, 
+      zip: zip, 
+      password:password,
+      email:email
+    }).then(()=> {
+      alert("Added successfully to Kolfall")
+    });
+  };
+  //Behöver lägga till/ta bost grejer för nya databasen
   return (
     <div className="App">
       <h1>Register</h1>
       <div className ="form">
         <label> Name </label>
-      <input type="text" name = "name" onChange={(e)=>{
-        setName(e.target.value)
+      <input type="text" name = "name" onChange={(event)=>{
+        setName(event.target.value)
+      }}/>
+      <label>  Adress </label>
+      <input type="text" name = "adress"onChange={(event)=>{
+        setEmail(event.target.value)
       }}/>
       <label> Email </label>
-      <input type="text" name = "adress"onChange={(e)=>{
-        setEmail(e.target.value)
-      }}/>
-      <label> Adress </label>
-      <input type="text" name = "email"onChange={(e)=>{
-        setAdress(e.target.value)
+      <input type="text" name = "email" onChange={(event)=>{
+        setAdress(event.target.value)
       }}/>
       <label> Zip </label>
-      <input type="text" name = "zip"onChange={(e)=>{
-        setZip(e.target.value)
+      <input type="number" name = "zip" onChange={(event)=>{
+        setZip(event.target.value)
       }}/>
       <label> Password </label>
-      <input type="text" name = "password"onChange={(e)=>{
-        setPassword(e.target.value)
+      <input type="text" name = "password" onChange={(event)=>{
+        setPassword(event.target.value)
       }}/>
       <label>Prosumer</label>
       <input type="checkbox" name = "checkbox" />
-      <button>onClick={submitRegistration} Submit</button>
+      <button onClick ={submitRegistration}> Submit</button>
       </div>
     </div>
   );
--- FrontEnd/server/index.js
+++ FrontEnd/server/index.js
@@ -1,29 +1,46 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
 //needs to be changed to connect to new database
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '6455593e.',
-    database: 'plsWork'
+    database: 'plsWork',
 });
 
-app.post('/api/insert', (req, res)=>{
 
-    const sqlInsert = "INSERT INTO user (name, adress, zip, password) VALUES (?,?,?,?)"
-    db.query(sqlInsert, [name, adress, zip, password], (err, result)=> {
-        
+app.get('/get', (req,res) => {
+    const sqlSelect = "SELECT * FROM user";
+    db.query(sqlSelect, (err, result) => {
+        console.log(result)
+
     });
+});
+app.post("/register", (req, res)=>{
+
+    const name = req.body.name;
+    const adress = req.body.adress;
+    const zip = req.body.zip;
+    const password = req.body.password;
+    const email = req.body.email;
+    //TODO fixa querry för ny databas
+    //const sqlInsert = "INSERT INTO user (name, email, adress, zip, password) VALUES (?,?,?,?,?)";
+    db.query("INSERT INTO user (name, adress, email, zip, password) VALUES (?,?,?,?,?)", 
+    [name, adress, email, zip, password], 
+    (err, result)=> {
+        if(err) {
+            console.log(err);
+        }else{
+            res.send("Values Inserted");
+        }
+    }
+    );
 
 });
-app.get('/', (req, res) =>{
-    const sqlInsert = "INSERT INTO user (name, adress, zip, password) VALUES ('inception', 'hej', 'test', 'test2');"
-    db.query(sqlInsert, (err, result)=>{
-        res.send('hello world')  
-    })
-});
-app.listen(3008, () => {
-    console.log("running on port 3008");
-});
+
+app.listen(3001);
