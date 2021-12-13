87f72d6b39442646c2957320c763ea02ff3defd2
Gustav-Rixon <Gustav.rixon@gmail.com>
2021-12-13 14:16:27 +0100

    Simulator now uses objects instead of a json file

--- kolfall/Simulator.py
+++ kolfall/Simulator.py
@@ -6,6 +6,16 @@ import json
 from mp1 import calc_station, calc_temp, calc_wind, calc_electricity_consumption
 
 
+class Household:
+    def __init__(self, id, wind, temp, consumption, closest_station_id, closest_station_distance):
+        self.id = id
+        self.wind = wind
+        self.temp = temp
+        self.consumption = consumption
+        self.closest_station_id = closest_station_id
+        self.closest_station_distance = closest_station_distance
+
+
 class Simulator:
     def __init__(self):
         self._wind = 1
@@ -37,43 +47,73 @@ class Simulator:
         p.start()
         self._temp = parent_conn.recv()
 
-    def setupSim(self):
+    # def setupSim(self):
         # TODO SHOULD CREATE AN JSON FILE WITH ALL HOUSE HOLDS
         # RIGHT NOW THIS DOES NOT WORK :(
-        print("setup go brrrrrr")
-        f = open('EventList.json',)
-        data = json.load(f)
-        f.close()
+    #    print("setup go brrrrrr")
+    #    f = open('EventList.json',)
+    #    data = json.load(f)
+    #    f.close()
 
-        for p in data['house_hold']:
-            if(p["data"][0]["closest_station_id"] == 0 and p["data"][0]["closest_station_distance"] == 0):
-                address = p["position"][0]["address"]
-                zipcode = p["position"][0]["zipcode"]
-                test = calc_station(address, zipcode)
-                p["data"][0]["closest_station_id"] = test[0]
-                p["data"][0]["closest_station_distance"] = test[1]
+    #    for p in data['house_hold']:
+    #        if(p["data"][0]["closest_station_id"] == 0 and p["data"][0]["closest_station_distance"] == 0):
+    #            address = p["position"][0]["address"]
+    #            zipcode = p["position"][0]["zipcode"]
+    #            test = calc_station(address, zipcode)
+    #            p["data"][0]["closest_station_id"] = test[0]
+    #            p["data"][0]["closest_station_distance"] = test[1]
 
-                f = open('EventList.json', "w")
-                f.write(json.dumps(p))
-                f.close()
+    #            f = open('EventList.json', "w")
+    #            f.write(json.dumps(p))
+    #            f.close()
+    # TEST USING A JSON FILE
+    # def run(self):
+    #    while True:
+    #        print("RUNNING")
+    #        with open('EventList.json', 'r+') as f:
+    #            data = json.load(f)
+    #            for p in data['house_hold']:
+    #                p["data"][0]["temp"] = calc_temp(
+    #                    p["data"][0]["closest_station_id"], p["data"][0]["closest_station_distance"])
+    #                p["data"][0]["wind"] = calc_wind(
+    #                    p["data"][0]["closest_station_id"], p["data"][0]["closest_station_distance"])
+    #                p["data"][0]["consumption"] = calc_electricity_consumption(
+    #                    p["data"][0]["temp"])
+    #                f.seek(0)
+    #                f.write(json.dumps(data))
+    #                f.truncate()
+    #        sleep(100)
+
+    # TODO:SHOULD GET ALL THE HOUSEHOLDS FROM THE DATABASE AND CREATE HOUSEHOLD OBJECTS
+    def setupSim(self):
+        h1 = Household(1, None, None, None, 97200, 4500)
+        h2 = Household(2, None, None, None, 162790, 3000)
+        h3 = Household(3, None, None, None, 162790, 100000)
+        household_list = []
+        household_list.append(h1)
+        household_list.append(h2)
+        household_list.append(h3)
+        print(household_list)
+        return household_list
 
     def run(self):
+        household_list = Simulator.setupSim(self)
         while True:
             print("RUNNING")
-            with open('EventList.json', 'r+') as f:
-                data = json.load(f)
-                for p in data['house_hold']:
-                    p["data"][0]["temp"] = calc_temp(
-                        p["data"][0]["closest_station_id"], p["data"][0]["closest_station_distance"])
-                    p["data"][0]["wind"] = calc_wind(
-                        p["data"][0]["closest_station_id"], p["data"][0]["closest_station_distance"])
-                    p["data"][0]["consumption"] = calc_electricity_consumption(
-                        p["data"][0]["temp"])
-                    f.seek(0)
-                    f.write(json.dumps(data))
-                    f.truncate()
-            sleep(100)
 
-            # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses
+            for household in household_list:
+                household.wind = calc_wind(
+                    household.closest_station_id, household.closest_station_distance)
+                household.temp = calc_temp(
+                    household.closest_station_id, household.closest_station_distance)
+                household.consumption = calc_electricity_consumption(
+                    household.temp)
 
-            #set_temp(0,"Strandv%C3%A4gen%205", "104%2040")
+                print(household.id, household.wind,
+                      household.temp,  household.consumption)
+
+            sleep(1)
+
+        # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses
+
+        # set_temp(0,"Strandv%C3%A4gen%205", "104%2040")
--- kolfall/mp2.py
+++ kolfall/mp2.py
@@ -32,7 +32,7 @@ def get_weather_data_wind(address, zipcode):
 
 if __name__ == "__main__":
     sim = Simulator()
-    sim.setupSim()
+    # sim.setupSim()
     x = threading.Thread(target=sim.run)
     x.start()
     app.run()
