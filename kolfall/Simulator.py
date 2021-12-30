27a8b93506be9dfdec3752ef1a1452dfda5f560b
Gustav-Rixon <Gustav.rixon@gmail.com>
2021-12-30 15:47:14 +0100

    Update Simulator.py

--- kolfall/Simulator.py
+++ kolfall/Simulator.py
@@ -6,17 +6,19 @@ from time import sleep
 from mp1 import send_info_wind, send_info_temp
 from resources.Rate_limited import rate_limited
 import json
-from mp1 import calc_station, calc_temp, calc_wind, calc_electricity_consumption
+from mp1 import calc_station, calc_temp, calc_wind, calc_electricity_consumption, calc_production
 from werkzeug.wrappers import Request, Response
 from multiprocessing import Process, Queue, Pipe
 from resources.GetDataFromExApi import get_data_from_station
 from Lorax import create_house_holds_objects, create_power_plants_objects
 from flask import Flask, json, jsonify
+from Market import Market
 
 global_household_list = []
 global_power_plant_list = []
 global_event_list = []
-fastpris = 10
+global_market = Market(0)
+fixed_price = 10
 
 app = Flask(__name__)
 
@@ -47,8 +49,20 @@ class Events:
         global global_power_plant_list
         print("Burning more hamsters!!!!")
         for power_plant in global_power_plant_list:
-            if power_plant.id == id:
-                power_plant.production = order
+            if power_plant._id == id:
+                power_plant._production = order
+
+    # event id 3 - blackout
+    def blackout_whole_network():
+        global global_household_list
+        for household in global_household_list:
+            household._power_status = 0
+
+    # event id 4 - blackout
+    def remove_blackout():
+        global global_household_list
+        for household in global_household_list:
+            household._power_status = 1
 
 
 class Simulator:
@@ -57,42 +71,78 @@ class Simulator:
         self._power_plants = []
         self._event_list = []  # TODO EVENTS? power out turbine explode?
         self._total_buffert = 0
+        self._simulator_market = 0
+        self._consumer_households_in_siumulation = []
+        self._prosumer_households_in_siumulation = []
 
     # TODO:SHOULD GET ALL THE HOUSEHOLDS FROM THE DATABASE AND CREATE HOUSEHOLD OBJECTS
     # kWh
     def setupSim():
-        household_list = create_house_holds_objects()
+        list_of_consumer, list_of_prosumer = create_house_holds_objects()
         power_plant_list = create_power_plants_objects()
-        return household_list, power_plant_list
+        return list_of_consumer, list_of_prosumer, power_plant_list
 
-    # y = kx+m, k = number of users, x = current consumtion, m = fast pris öre/kWh
-    def calculate_electricity_price(current_consumption, current_buffert):
-        global global_household_list
-        global fastpris
+    def calc_consumer(list_of_consumer):
+        total_consumer_consumption = 0
+        for consumer in list_of_consumer:
+            consumer._temp = calc_temp(
+                consumer._closest_station_id, consumer._closest_station_distance)
+            consumer._consumption = calc_electricity_consumption(
+                consumer._temp)
+            total_consumer_consumption += consumer._consumption
+        return total_consumer_consumption
 
-        if ((len(global_household_list)*current_consumption) + fastpris - current_buffert <= fastpris):
-            return fastpris
-        else:
-            return (len(global_household_list)*current_consumption) + fastpris - current_buffert
+    def calc_prosumer(list_of_prosumer):
+        total_prosumer_consumption = 0
+        total_prosumer_production = 0
+        for prosumer in list_of_prosumer:
+            prosumer._wind = calc_wind(
+                prosumer._closest_station_id, prosumer._closest_station_distance)
+            prosumer._production = calc_production(
+                prosumer._wind)*prosumer._ratio_to_market
+            prosumer._buffert.content += prosumer._production * \
+                (1-prosumer._ratio_to_market)
+            prosumer._temp = calc_temp(
+                prosumer._closest_station_id, prosumer._closest_station_distance)
+            prosumer._consumption = calc_electricity_consumption(
+                prosumer._temp)
 
+            if prosumer._buffert.content < prosumer._consumption:
+                prosumer._consumption -= prosumer._buffert.content
+                prosumer._buffert.content = 0
+
+            if prosumer._buffert.content > prosumer._consumption:
+                prosumer._buffert.content -= prosumer._consumption
+                prosumer._consumption = 0
+
+            total_prosumer_consumption += prosumer._consumption
+            total_prosumer_production += prosumer._production
+        return total_prosumer_consumption, total_prosumer_production
+
+    def calc_power_plant_production(list_of_power_plants):
+        total_production = 0
+        for power_plant in list_of_power_plants:
+            total_production += power_plant._production
+        return total_production
+
+    # TODO BE ABLE TO ADD NEW USERS TO THE LOOP AND REMOVE THEM
     def run(self):
-        self._household_list_in_siumulation, self._power_plant = Simulator.setupSim()
-        # self._event_list.append(Events.lightsOut)
-        # self._event_list.append(Events.lightsOn)
+        self._consumer_households_in_siumulation, self._prosumer_households_in_siumulation, self._power_plant = Simulator.setupSim()
+        self._simulator_market = Market(1000)
 
         global global_household_list
         global global_power_plant_list
         global global_event_list
-        global_household_list = self._household_list_in_siumulation
+        global global_market
+
+        global_household_list = self._consumer_households_in_siumulation + \
+            self._prosumer_households_in_siumulation
         global_power_plant_list = self._power_plant
+        global_market = self._simulator_market
 
         # global_event_list.append(Events.change_production)
 
         while True:
-            current_consumption = 0
-            current_production = 0
-            total_current_production = 0
-
             print("RUNNING")
 
             # TODO HAVE A LIST OR INSTAND ACTION
@@ -101,53 +151,59 @@ class Simulator:
             #        event()  # pre defined varibels pls
             #        global_event_list.pop(0)
 
-            for household in self._household_list_in_siumulation:
-                household.wind = calc_wind(
-                    household.closest_station_id, household.closest_station_distance)
-                household.temp = calc_temp(
-                    household.closest_station_id, household.closest_station_distance)
-                household.consumption = calc_electricity_consumption(
-                    household.temp)
+            simulator_consumption, simulator_production = Simulator.calc_prosumer(
+                self._prosumer_households_in_siumulation)
 
-                print(household.id, household.wind,
-                      household.temp,  household.consumption, household.power_status)
-                current_consumption += household.consumption
+            simulator_consumption += Simulator.calc_consumer(
+                self._consumer_households_in_siumulation)
 
-            #############################Powerplant and buffert#######################################
+            simulator_production += Simulator.calc_power_plant_production(
+                self._power_plant)
 
-            # I CRY want to calculate what the networks capacity is
-            for power_plant in self._power_plant:
+            market_status = self._simulator_market.update_market(
+                simulator_production-simulator_consumption)
 
-                print("#############current_production from reactor #############")
-                print(power_plant.production)
-                print("#############current_buffert in reactor #############")
-                print(power_plant.buffert.content)
+            #################################################
 
-                #############################Powerplant and buffert#######################################
+            if self._simulator_market.market_buffert.content == 0:
+                Events.blackout_whole_network()
+
+            if self._simulator_market.market_buffert.content > 0:
+                Events.remove_blackout()
+
+            for object in self._consumer_households_in_siumulation:
+                print(
+                    f"CONSUMER id:{object._id} status:{object._power_status}")
+
+            for object in self._prosumer_households_in_siumulation:
+                object.power_check()
+                print(
+                    f"PROSUMER id:{object._id} status:{object._power_status} buffert:{object._buffert.content} consumption:{object._consumption}")
+
+            #################################################
+
+            print(f"markut status {market_status}")
 
             print("############current_consumption##############")
-            print(current_consumption)
-            print("###########################")
-            print(f"buffert is {self._total_buffert}")
-            print(Simulator.calculate_electricity_price(
-                current_consumption, self._total_buffert))
+            print(simulator_consumption)
             print("#############current_production total#############")
-            print(power_plant.production)
+            print(simulator_production)
             print("#############current_net#############")
-            print(total_current_production-current_consumption)
+            print(simulator_production-simulator_consumption)
+            print(
+                f"????????????????? {global_market.market_buffert.content} ?????????????????")
             sleep(1)
 
         # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses
         # set_temp(0,"Strandv%C3%A4gen%205", "104%2040")
 
 
-class API_ENDPOINTS:
+class SimulatorEndPoints:
     @app.route('/DATA/house_hold/consumption/house_hold=<int:id>', methods=['GET'])
     def get_house_hold_consumption(id):
         global global_household_list
-
         for house_hold in global_household_list:
-            if house_hold.id == id:
+            if house_hold._id == id:
                 return str(house_hold.consumption)
             else:
                 return "do not finns"
@@ -155,19 +211,48 @@ class API_ENDPOINTS:
     # ONLY WORKS FOR ONE EVENT
     @app.route('/admin/tools/change_power/id=<int:id>&power=<int:power>', methods=['POST'])
     @rate_limited(1/10, mode='kill')
-    def change_power(id, power):
+    def change_power_plant_output(id, power):
         Events.change_production(id, power)
         return f"burning {power} hamsters insted"
+
+    # ONLY WORKS FOR ONE EVENT
+    @app.route('/admin/tools/change_market_size/size=<int:size>', methods=['POST'])
+    @rate_limited(1/10, mode='kill')
+    def change_market_size(size):
+        Events.change_market_size(size)
+        return f"Changning market size to {size} hamsters insted"
+
+    @app.route('/sell/house_hold/prosumer/house_hold=<int:id>&amount=<int:amount>', methods=['POST'])
+    def sell(id, amount):
+        global global_market
+        for house_hold in global_household_list:
+            if house_hold._id == id:
+                if amount < house_hold._buffert.content:
+                    house_hold._buffert.content -= amount
+                    Market.send_to_market(global_market, amount)
+                    return (f"SUCC {global_market.market_buffert.content}")
+                return (f"FAIL {house_hold._buffert.content}")
+
+    @app.route('/buy/house_hold/prosumer/house_hold=<int:id>&amount=<int:amount>', methods=['POST'])
+    def buy(id, amount):
+        global global_market
+        for house_hold in global_household_list:
+            if house_hold._id == id:
+                if amount < global_market.market_buffert.content:
+                    house_hold._buffert.content += amount
+                    Market.buy_from_market(global_market, amount)
+                    return (f"SUCC {global_market.market_buffert.content}")
+                return (f"FAIL {global_market.market_buffert.content}")
 
 
 if __name__ == "__main__":
     sim = Simulator()
-    #smhi = get_data_from_station()
+    # smhi = get_data_from_station()
 
     x = threading.Thread(target=sim.run)
     x.daemon = True
     x.start()
-    #y = threading.Thread(target=smhi.update_data)
-    #y.daemon = True
+    # y = threading.Thread(target=smhi.update_data)
+    # y.daemon = True
     # y.start()
     app.run()  # Main thred
