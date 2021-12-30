aa57c76c393106daf4a22f1ddeda2c992d6f92da
Gustav-Rixon <Gustav.rixon@gmail.com>
2021-12-30 15:46:47 +0100

    update to market

--- kolfall/Market.py
+++ kolfall/Market.py
@@ -1,19 +1,36 @@
+from Lorax import Buffert, Prosumer
+
+
 class Market:
     def __init__(self, market_size):
-        self.market_buffert = 0  # The market is empty when started
-        self.market_size = market_size
+        # The market is empty when started
+        self.market_buffert = Buffert(market_size, 0)
         self.market_price = 100  # Starting market price
 
-    def send_to_market(self, amount):
-        self.market_buffert += amount
+    # CAN BE pos and neg
+    def update_market(self, amount):
+        self.market_buffert.content += amount
+        Buffert.buffert_checker(self.market_buffert)
 
+        if self.market_buffert.content <= 0:
+            self.market_buffert.content = 0
+
+    # ONLY POS
+    def send_to_market(self, amount):
+        self.market_buffert.content += amount
+
+    # ONLY NEG
     def buy_from_market(self, amount):
-        self.market_buffert -= amount
+        self.market_buffert.content -= amount
+
+    def change_market_size(self, amount):
+        self.market_buffert.capacity = amount
 
     # y = kx+m, k = number of users, x = current consumtion, m = fast pris öre/kWh
+
     def calculate_electricity_price(self, current_consumption, number_of_users, fixed_price):
-        if ((number_of_users*current_consumption) + fixed_price - self.market_buffert <= fixed_price):
+        if ((number_of_users*current_consumption) + fixed_price - self.market_buffert.content <= fixed_price):
             self.market_price = fixed_price
         else:
             self.market_price = (
-                number_of_users*current_consumption) + fixed_price - self.market_buffert
+                number_of_users*current_consumption) + fixed_price - self.market_buffert.content
