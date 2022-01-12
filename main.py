16cd69f247b716d0975394f2beae7f5a338ded46
Gustav-Rixon <Gustav.rixon@gmail.com>
2022-01-12 22:06:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Backend import Simulator
+from Backend.resources import GetDataFromSMHIApi
 import threading
 
 if __name__ == "__main__":
@@ -6,11 +7,14 @@ if __name__ == "__main__":
 
     """
     sim = Simulator.Simulator()
-    #smhi = get_data_from_station()
+    smhi = GetDataFromSMHIApi.get_data_from_station()
 
-    x = threading.Thread(target=sim.run)
+    x = threading.Thread(target=sim.run(debugmode=True))
     x.daemon = True
     x.start()
+    y = threading.Thread(target=smhi.update_data(debugmode=True))
+    y.daemon = True
+    y.start()
 
     from werkzeug.serving import run_simple
     run_simple('127.0.0.1', 5000, Simulator.SimulatorEndPoints.application)
